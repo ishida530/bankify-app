456a2c70166e253f6c42395263f559a2269e30cf
ishida530 <ishida530@gmail.com>
2024-05-19 12:46:23 +0200

    fix wrong path to transaction

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,7 +1,6 @@
 import HeaderBox from '@/components/HeaderBox'
 import { Pagination } from '@/components/Pagination';
 import TransactionsTable from '@/components/TransactionsTable';
-import { useAccountsDetails } from '@/hooks/useAccountsDetails';
 import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
 import { formatAmount } from '@/lib/utils';
@@ -21,13 +20,13 @@ const TransactionHistory = async ({ searchParams: { id, page } }: SearchParamPro
   const accountsData = accounts?.data;
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
   const account = await getAccount({ appwriteItemId })
-
   const rowsPerPage = 10;
-  const totalPages = Math.ceil(account.length / rowsPerPage)
+  const totalPages = Math.ceil(account?.length / rowsPerPage)
   const indexOfLastTransaction = currentPage * rowsPerPage
   const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage
-  const currentTransactions = account.slice(indexOfFirstTransaction, indexOfLastTransaction)
+  const currentTransactions = account?.transactions.slice(indexOfFirstTransaction, indexOfLastTransaction)
 
+  
   return (
     <Suspense fallback={<Loading />}>
       <div className='transactions'>
